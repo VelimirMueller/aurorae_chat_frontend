e7ddac51410b0b8e7f396dcc94bbd5fb9b0a8d72
velimir <velimir.mueller@googlemail.com>
2024-01-05 13:15:09 +0100

    adjust navbar layout and add logo to navbar

--- components/Layout/NavbarSection.vue
+++ components/Layout/NavbarSection.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="h-12">
-    <ul class="flex flex-row gap-4">
-      <li>logo</li>
-      <li>navitem1</li>
-      <li>navitem1</li>
-      <li>navitem1</li>
-      <li>button</li>
+  <div class="h-12 flex">
+    <a class="flex items-center h-full" href="#">
+      <img class="w-12" src="~/assets/img/aurorae-fav-logo.png">
+      <p class="w-60 text-m font-bold">The Aurorae Project</p>
+    </a>
+    <ul class="flex flex-row gap-4 h-full justify-center space-between flex-wrap items-center w-full">
+      <!-- Navigation Items -->
+      <li class="justify-between flex">
+        nav-item-1
+      </li>
+      <li>nav-item-2</li>
+      <li>nav-item-3</li>
     </ul>
+    <span class="flex items-center h-full space-x-3 w-40 flex justify-center items-center">
+      <button>click me</button>
+    </span>
   </div>
 </template>
