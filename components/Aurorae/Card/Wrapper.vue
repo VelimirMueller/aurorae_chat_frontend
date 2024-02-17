13c1eb5f72b099dec57efcb5cf4f9bc44220b51b
velimir <velimir.mueller@googlemail.com>
2024-02-18 00:10:43 +0100

    Refactor: git status

---
+++ components/Aurorae/Card/Wrapper.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    v-if="!isMenuWrapper"
+    id="cardWrapper"
+    class="flex flex-col h-full">
+    <div :class="`h-1/6 ${ props.isBg ? 'bg-card-header bg-cover' : '' }`">
+      <slot name="card-header" />
+    </div>
+    <div class="h-4/6">
+      <slot name="card-body" />
+    </div>
+    <div class="h-1/6">
+      <slot name="card-footer" />
+    </div>
+  </div>
+  <div
+    v-else
+    class="flex flex-col h-full">
+    <div class="h-full">
+      <slot name="side-menu" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  isBg: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+  isMenuWrapper: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+})
+</script>
