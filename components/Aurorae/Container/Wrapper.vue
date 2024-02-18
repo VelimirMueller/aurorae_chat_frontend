37cfbd9efc8965befe462ed64db1052d0fd7b089
velimir <velimir.mueller@googlemail.com>
2024-02-18 01:49:29 +0100

    rename button on index page

--- components/Aurorae/Container/Wrapper.vue
+++ components/Aurorae/Container/Wrapper.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="w-full h-full flex justify-center items-center">
-    <div class="flex flex-row px-12 md:px-0 w-full h-full content-center justify-center">
+  <div :class="props.containerType === 'page' ? containerDefaults.container : 'hidden'">
+    <div :class="props.containerType === 'page' ? containerDefaults.content : 'hidden' ">
       <slot name="content" />
     </div>
   </div>
 </template>
 
-<script>
-export default defineComponent({
-  name: 'AuroraeContainer'
+<script setup lang="ts">
+
+const props = defineProps({
+  containerType: {
+    type: String,
+    required: false,
+    default: 'page'
+  }
 })
+const containerDefaults = {
+  container: 'w-full h-full flex justify-center items-center',
+  content: 'flex flex-row px-12 md:px-0 w-full h-full content-center justify-center'
+}
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -13,18 +13,23 @@
               <AuroraeBlockquoteWrapper
                 headline="What is Aurorae?"
                 :highlight-headline="true">
-                The name "Aurorae" is a beautiful and poetic name that refers to the natural light display
-                that occurs in the polar regions.
-                <br><br>
-                It is named after the Roman goddess of the dawn, Aurora,
-                who was believed to bring joy and happiness.
+                <div>
+                  <p class="p-2 my-4">
+                    The name "Aurorae" is a beautiful and poetic name that refers to the natural light display
+                    that occurs in the polar regions.
+                  </p>
+                  <p class="p-2 my-4">
+                    It is named after the Roman goddess of the dawn, Aurora,
+                    who was believed to bring joy and happiness.
+                  </p>
+                </div>
               </AuroraeBlockquoteWrapper>
             </aside>
           </template>
           <template #card-footer>
             <div class="flex flex-row w-full justify-center items-center h-full pb-8">
-              <ButtonCta class="w-1/3 mr-2" />
-              <ButtonCta class="w-1/3" />
+              <AuroraeButtonTypeCta class="w-1/3 mr-2" />
+              <AuroraeButtonTypeCta class="w-1/3" />
             </div>
           </template>
         </AuroraeCardWrapper>
