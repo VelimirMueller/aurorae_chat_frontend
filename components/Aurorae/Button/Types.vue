4050a53f581f2b34b2e3ec795c4e5a6ca21baf06
velimir <velimir.mueller@googlemail.com>
2024-02-19 22:01:29 +0100

    add scss support

--- components/Aurorae/Button/Types.vue
+++ components/Aurorae/Button/Types.vue
@@ -29,8 +29,8 @@ const props = defineProps({
 })
 
 const types = {
-  default: 'w-full h-12 opacity-100 border-2 border-gray-900 rounded-lg active:bg-cyan-100 active:text-gray-800 active:border-gray-900 bg-white',
-  submit: `w-full mt-2 mr-2 ml-auto h-4/6 bg-gray-700 text-gray-200 hover:bg-gray-500 hover:text-gray-300 hover:border-gray-300 border-2 active:bg-cyan-100 active:text-gray-700 active:border-cyan-400 rounded-md ${props.isLoading ? 'bg-cyan-900 text-gray-100 pointer-events-none' : ''}`,
-  reset: `w-full mt-2 mr-2 ml-auto h-4/6 bg-gray-200 text-gray-900 hover:bg-gray-300 border-2 hover:text-gray-800 hover:border-gray-600 active:bg-cyan-100 rounded-md active:bg-cyan-100 active:text-gray-800 active:border-cyan-400 ${props.isLoading ? 'bg-gray-700 text-gray-400 pointer-events-none' : ''}`
+  default: 'btn-outline',
+  submit: `btn-primary ${props.isLoading ? 'pointer-events-none opacity-50' : ''}`,
+  reset: `btn-secondary ${props.isLoading ? 'pointer-events-none opacity-50' : ''}`
 }
 </script>
